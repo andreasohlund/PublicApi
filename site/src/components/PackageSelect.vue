bf563e146ede47c7ec91f98540128f8a896286c3
Andreas Öhlund <andreas.ohlund@particular.net>
2019-05-26 15:36:18 +0200

    Wire up search

--- site/src/components/PackageSelect.vue
+++ site/src/components/PackageSelect.vue
@@ -1,14 +1,34 @@
 <template>
-  <v-autocomplete
-    v-model="model"
-    :items="packages"
-    label="Select NuGet package"
-  >
-  </v-autocomplete>
+  <v-autocomplete 
+  v-model="model" 
+  :items="packages" 
+  label="Select NuGet package"
+  :search-input.sync="search"
+  ></v-autocomplete>
 </template>
 
 <script>
 export default {
-  data: () => ({})
+  data: () => {
+    return {
+      model: null,
+      search: null,
+      isLoading: false,
+      packages: []
+    };
+  },
+   watch: {
+    search (val) {
+          console.log(val);
+      
+      // Items have already been loaded
+      //if (this.items.length > 0) return
+
+      // Items have already been requested
+      if (this.isLoading) return
+
+      this.isLoading = true
+    }
+  }
 };
 </script>
