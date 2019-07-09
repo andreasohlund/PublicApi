cbcabd7b8e79a226d8d7df5ee947635c72f9130f
Andreas Öhlund <andreas.ohlund@particular.net>
2019-07-09 11:11:29 +0200

    Detect indexing need

--- site/src/components/ViewPackage.vue
+++ site/src/components/ViewPackage.vue
@@ -1,17 +1,35 @@
 <template>
-  <div><h2>{{ id }} - {{ version }}</h2></div>
+  <div>
+          <v-card v-if="needsindexing">
+    <v-card-title class="headline lighten-3">{{ id }} - {{ version }} not indexed yet</v-card-title>
+    <v-card-text >Please wait while we index the package</v-card-text>
+  </v-card>
+    
+    </div>
 </template>
 
 <script>
 export default {
+   data: () => {
+    return {
+      needsindexing: false
+    };
+  },
   props: ['id','version'],
   mounted: function (){
     console.log(`mounted: ${this.id}/${this.version}.json`)
 
     let packageApiDetails = this.$packageApi.get(`${this.id}/${this.version}.json`)
-      .then(response => resonse.data);
-     
-    console.log("packageApiDetails");
+      .then(response => resonse.data)
+      .catch(error =>{
+        if(error.response && error.response.status == 404)
+        {
+          this.needsindexing = true;
+          return;
+        }
+
+        throw error;
+      });
   }
 };
 </script>
