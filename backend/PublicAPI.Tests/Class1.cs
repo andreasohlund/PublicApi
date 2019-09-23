957eafc52f55d0ebbeda2699f206702d0aa652e0
Andreas Öhlund <andreas.ohlund@particular.net>
2019-09-23 19:09:21 +0200

    Spike extracting nuget package

--- backend/PublicAPI.Tests/Class1.cs
+++ backend/PublicAPI.Tests/Class1.cs
@@ -1,13 +1,30 @@
 ﻿using NUnit.Framework;
+using System;
+using System.IO.Compression;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace PublicAPI.Tests
 {
     public class Class1
     {
         [Test]
-        public void Testit()
+        public async Task Testit()
         {
-            
+            var client = new HttpClient();
+
+            var response = await client.GetAsync("https://api.nuget.org/v3-flatcontainer/newtonsoft.json/9.0.1/newtonsoft.json.9.0.1.nupkg");
+            response.EnsureSuccessStatusCode();
+
+            using (var responseStream = await response.Content.ReadAsStreamAsync())
+            using (var archive = new ZipArchive(responseStream))
+            {
+                foreach (var entry in archive.Entries)
+                {
+                    Console.WriteLine(entry.FullName);
+                }
+            }
+      
         }
 
     }
