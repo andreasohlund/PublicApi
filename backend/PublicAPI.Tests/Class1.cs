5c40fee446aed8cd1a264950e824c8f0edb4a217
Andreas Öhlund <andreas.ohlund@particular.net>
2019-09-24 21:42:49 +0200

    Extract types test

--- backend/PublicAPI.Tests/Class1.cs
+++ backend/PublicAPI.Tests/Class1.cs
@@ -1,6 +1,10 @@
-﻿using NUnit.Framework;
+﻿using Mono.Cecil;
+using NUnit.Framework;
 using System;
+using System.IO;
 using System.IO.Compression;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -21,10 +25,40 @@ namespace PublicAPI.Tests
             {
                 foreach (var entry in archive.Entries)
                 {
-                    Console.WriteLine(entry.FullName);
+                    var path = WebUtility.UrlDecode(entry.FullName).ToLower();
+                    if (path.StartsWith("lib/") && path.EndsWith(".dll"))
+                    {
+                        using (var asmStream = entry.Open())
+                        using (var memStream = new MemoryStream())
+                        {
+                            //copy to a mem stream since cecil needs the stream to be seekable
+                            await asmStream.CopyToAsync(memStream);
+
+                            memStream.Position = 0;
+                            var assembly = AssemblyDefinition.ReadAssembly(memStream);
+
+                            var publicTypes = assembly.Modules.SelectMany(m => m.GetTypes())
+                                .Where(t => !t.IsNested && ShouldIncludeType(t))
+                                .OrderBy(t => t.FullName, StringComparer.Ordinal);
+
+                            Console.WriteLine(path + " - " + publicTypes.Count());
+                        }
+                            
+                    }
+                    
                 }
             }
       
+        }
+
+        static bool ShouldIncludeType(TypeDefinition t)
+        {
+            return (t.IsPublic || t.IsNestedPublic || t.IsNestedFamily) && !IsCompilerGenerated(t);
+        }
+
+        static bool IsCompilerGenerated(IMemberDefinition m)
+        {
+            return m.CustomAttributes.Any(a => a.AttributeType.FullName == "System.Runtime.CompilerServices.CompilerGeneratedAttribute");
         }
 
     }
