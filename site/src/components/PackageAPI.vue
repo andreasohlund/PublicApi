35c2b4e52a3b1379040e3166d700bd228b037736
Andreas Öhlund <andreas.ohlund@particular.net>
2019-11-24 11:13:50 +0100

    Expansion panels

--- site/src/components/PackageAPI.vue
+++ site/src/components/PackageAPI.vue
@@ -1,19 +1,29 @@
 <template>
   <v-card>
-    <v-card-title class="headline lighten-3">Target frameworks</v-card-title>
-    <v-tabs center-active dark>
-      <v-tab v-for="tfm in packageDetails.TargetFrameworks" v-bind:key="tfm.Name">{{tfm.Name}}</v-tab>
-      <v-tab-item v-for="tfm in packageDetails.TargetFrameworks" v-bind:key="tfm.Name">
-        <v-card v-for="assembly in tfm.Assemblies" v-bind:key="tfm.Name + '-' + assembly.Name">
-          <v-card>
-            <v-card
-              v-for="type in assembly.PublicTypes"
-              v-bind:key="tfm.Name + '-'+ assembly.Name + '-' + type.Namespace +'-' + type.Name"
-            >{{type.Namespace}}.{{type.Name}}</v-card>
+    <v-card-title class="headline lighten-3">Public types</v-card-title>
+    <v-card-text>
+      <v-tabs center-active dark>
+        <v-tab v-for="tfm in packageDetails.TargetFrameworks" v-bind:key="tfm.Name">{{tfm.Name}}</v-tab>
+        <v-tab-item v-for="tfm in packageDetails.TargetFrameworks" v-bind:key="tfm.Name">
+          <v-card v-for="assembly in tfm.Assemblies" v-bind:key="tfm.Name + '-' + assembly.Name">
+            <v-expansion-panels inset multiple>
+              <v-expansion-panel
+                v-for="type in assembly.PublicTypes"
+                v-bind:key="tfm.Name + '-'+ assembly.Name + '-' + type.Namespace +'-' + type.Name"
+              >
+                <v-expansion-panel-header>
+                  <div>
+                    <span class="font-italic font-weight-light">{{type.Namespace}}.</span>
+                    <span class="font-weight-bold">{{type.Name}}</span>
+                  </div>
+                </v-expansion-panel-header>
+                <v-expansion-panel-content>Coming soon</v-expansion-panel-content>
+              </v-expansion-panel>
+            </v-expansion-panels>
           </v-card>
-        </v-card>
-      </v-tab-item>
-    </v-tabs>
+        </v-tab-item>
+      </v-tabs>
+    </v-card-text>
   </v-card>
 </template>
 
