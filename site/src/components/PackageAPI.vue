e383da46775f5abe98cf3dd6de269ba31eb5ebf0
Andreas Öhlund <andreas.ohlund@particular.net>
2019-11-27 22:58:19 +0100

    Formatting

--- site/src/components/PackageAPI.vue
+++ site/src/components/PackageAPI.vue
@@ -1,6 +1,10 @@
 <template>
   <v-card>
-    <v-card-title class="headline lighten-3">Public types</v-card-title>
+    <v-card-title class="headline lighten-3">
+      Public types
+      <v-spacer></v-spacer>
+      <v-chip outlined color="primary" v-for="tfm in this.targetFrameworks" v-bind:key="tfm">{{tfm}}</v-chip>
+    </v-card-title>
     <v-card-text>
       <v-expansion-panels multiple>
         <v-expansion-panel v-for="type in this.allTypes" v-bind:key="type.Id">
@@ -10,12 +14,7 @@
               <span class="font-weight-bold">{{type.Name}}</span>
             </div>
           </v-expansion-panel-header>
-          <v-expansion-panel-content>
-            <v-chip
-              v-for="availability in type.AvailableIn"
-              v-bind:key="availability.Framework"
-            >{{availability.Framework}}</v-chip>
-          </v-expansion-panel-content>
+          <v-expansion-panel-content>TODO</v-expansion-panel-content>
         </v-expansion-panel>
       </v-expansion-panels>
     </v-card-text>
@@ -28,18 +27,23 @@ export default {
   data: () => {
     return {
       namespaces: null,
-      allTypes: null
+      allTypes: null,
+      targetFrameworks: null
     };
   },
   mounted: function() {
     let types = {};
     let tfms = new Set();
 
+    let namespaces = new Set();
+
     this.packageDetails.TargetFrameworks.forEach(tfm => {
       tfms.add(tfm.Name);
 
       tfm.Assemblies.forEach(assembly => {
         assembly.PublicTypes.forEach(type => {
+          namespaces.add(type.Namespace);
+
           let id = `${assembly.Name}-${type.Namespace}.${type.Name}`;
 
           let existingType = types[id];
@@ -64,6 +68,8 @@ export default {
     // window.console.log(tfms);
     // window.console.log(types);
     this.allTypes = types;
+    this.targetFrameworks = tfms;
+    this.namespaces = namespaces;
   }
 };
 </script>
